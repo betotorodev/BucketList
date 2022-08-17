9e2881f910de16096141305fa80c982e67194691
Beto Toro <betotoro0902@gmail.com>
2022-08-17 14:48:10 -0500

    [feat]: integrate the first layout of the app

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -5,16 +5,47 @@
 //  Created by Beto Toro on 16/08/22.
 //
 
+import MapKit
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-      FaceIdView()
+  @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 50, longitude: 0), span: MKCoordinateSpan(latitudeDelta: 25, longitudeDelta: 25))
+  @State private var locations = [Location]()
+  
+  var body: some View {
+    ZStack {
+      Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
+        MapMarker(coordinate: CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude), tint: Color.green)
+      }
+        .ignoresSafeArea()
+      Circle()
+        .fill(.blue)
+        .opacity(0.3)
+        .frame(width: 32, height: 32)
+      VStack {
+        Spacer()
+        HStack {
+          Spacer()
+          Button {
+            let newLocation = Location(id: UUID(), name: "New location", description: "", latitude: mapRegion.center.latitude, longitude: mapRegion.center.longitude)
+            locations.append(newLocation)
+          } label: {
+            Image(systemName: "plus")
+          }
+          .padding()
+          .background(.black.opacity(0.75))
+          .foregroundColor(.white)
+          .font(.title)
+          .clipShape(Circle())
+          .padding(.trailing)
+        }
+      }
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
