2392d5a1cb3cc66bc7aecce9f8ec96329d71505e
Beto Toro <betotoro0902@gmail.com>
2022-08-24 11:28:36 -0500

    [feat]: second and third challenge done by teacher

--- BucketList/views/EditView.swift
+++ BucketList/views/EditView.swift
@@ -8,49 +8,21 @@
 import SwiftUI
 
 struct EditView: View {
-//  enum LoadingState {
-//    case loading, loaded, failed
-//  }
   @Environment(\.dismiss) var dismiss
-  @StateObject private var viewModel = ViewModel()
-  var location: Location
+  @StateObject private var viewModel: ViewModel
   var onSave: (Location) -> Void
   
   init(location: Location, onSave: @escaping (Location) -> Void) {
-    self.location = location
     self.onSave = onSave
-    
-  }
-  
-  func fetchNearbyPlaces() async {
-    let urlString = "https://en.wikipedia.org/w/api.php?ggscoord=\(location.coordinate.latitude)%7C\(location.coordinate.longitude)&action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500&pilimit=50&wbptterms=description&generator=geosearch&ggsradius=10000&ggslimit=50&format=json"
-    
-    guard let url = URL(string: urlString) else {
-      print("Bad URL: \(urlString)")
-      return
-    }
-    
-    do {
-      let (data, _) = try await URLSession.shared.data(from: url)
-      
-      // we got some data back!
-      let items = try JSONDecoder().decode(Result.self, from: data)
-      
-      // success – convert the array values to our pages array
-      viewModel.pages = items.query.pages.values.sorted()
-      viewModel.loadingState = .loaded
-    } catch {
-      // if we're still here it means the request failed somehow
-      viewModel.loadingState = .failed
-    }
+    _viewModel = StateObject(wrappedValue: ViewModel(location: location))
   }
   
   var body: some View {
     NavigationView {
       Form {
         Section {
-          TextField("Place name", text: $name)
-          TextField("Description", text: $description)
+          TextField("Place name", text: $viewModel.name)
+          TextField("Description", text: $viewModel.description)
         }
         Section("Nearby…") {
           switch viewModel.loadingState {
@@ -72,17 +44,13 @@ struct EditView: View {
       .navigationTitle("Place details")
       .toolbar {
         Button("Save") {
-          var newLocation = location
-          newLocation.id = UUID()
-          newLocation.name = name
-          newLocation.description = description
-          
+          let newLocation = viewModel.createNewLocation()
           onSave(newLocation)
           dismiss()
         }
       }
       .task {
-        await fetchNearbyPlaces()
+        await viewModel.fetchNearbyPlaces()
       }
     }
   }
